c27bbbcc9192c2d4b8765c2d5d5a9a012fe325db
Lagarware <carloslmram@gmail.com>
2022-04-18 13:25:42 -0600

    auth0

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-h7j1s0gy.us.auth0.com"
+      clientId="SdKy0JwGzn8keKmiDdFcS3cNTZy3fiDx"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+    
   </React.StrictMode>
 );
 
